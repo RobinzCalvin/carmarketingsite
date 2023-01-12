3fee2792a2a5120247addc1d2186841ab77b7719
Robinz Calvin <robinzcalvin@gmail.com>
2023-01-11 20:03:42 -0500

    filter updated

---
+++ src/component/BasicDateRangePicker.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+export default function BasicDateRangePicker() {
+  const [value, setValue] = React.useState([null, null]);
+
+  return (
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      localeText={{ start: 'Check-in', end: 'Check-out' }}
+    >
+      <DateRangePicker
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(startProps, endProps) => (
+          <React.Fragment>
+            <TextField {...startProps }  className='text' />
+            <Box sx={{ mx: 2 }}> to </Box> 
+            <TextField {...endProps} className='text' />
+          </React.Fragment>
+        )}
+      />
+    </LocalizationProvider>
+  );
+}
--- src/layout/main/filters.js
+++ src/layout/main/filters.js
@@ -1,7 +1,13 @@
+import { Input, Radio, RadioGroup } from '@mui/material';
+import BaseComponent from 'bootstrap/js/dist/base-component';
 import React,{ useState} from 'react';
+import BasicDateRangePicker from '../../component/BasicDateRangePicker';
+import MinimumDistanceSlider from '../../component/minimumDistanceSlider';
+
 export function Filters() {
   const [sLists,setSList]=useState([]);
   const [oList,setOList]=useState([]);
+  const minDistance = 10;
 
 
   let natLists=["Olive oil","Oregano","tomato","olives"];
@@ -9,6 +15,10 @@ export function Filters() {
       title:"Nutrition details",
       detail:natLists,
   };
+  function valuetext(value) {
+    return `${value}°C`;
+  }
+  
   const handleApply =() => {///////Include ingredient condition
     // let s="";
     // sLists.map((sdata, index) => s=s+sdata+',');
@@ -23,12 +33,6 @@ export function Filters() {
         <div className='  w-5/6 float-left'>
           <div className="box-border h-auto w-full  border-1 rounded-lg">
             <p className='pt-5 font-bold text-xl  '>Filters</p>            
-            {/* <select className="langsel2">
-              <option value={1}>english</option>
-              <option value={1}>russion</option>
-              <option value={1}>english</option>
-              <option value={1}>english</option>
-            </select> */}
             <p className='font-semibold mb-0 mt-2	'>Sort by</p>
             <select className="langsel2" placeholder='Select...'>
               <option value={1}>Auction Date</option>
@@ -52,8 +56,24 @@ export function Filters() {
               <option value={1}>Copart</option>
               <option value={1}>IAAI</option>
               
-            </select>       
-          </div>          
+            </select>  
+            <div className='w-full flex flex-row pt-2'>
+              <RadioGroup>
+                  <Radio value={"Copart"}>Copart</Radio>
+                  <Radio value={'IAAI'}>IAAI</Radio>
+              </RadioGroup>
+            </div>
+            <div className='w-full pt-2'>
+              <MinimumDistanceSlider/>
+            </div>
+            <div className='w-full flex flex-row pt-2'>
+              <Input className='w-auto m-1' type='text' value={'1960'}/>
+              <Input className='w-auto m-1' type='text' value={'2022'}/>
+            </div>
+            <div className='w-full pt-2'>
+              <BasicDateRangePicker/>
+            </div>
+            </div>          
           <button className='rounded-md w-full h-10 bg-[#F29F05] text-2xl text-white hover:bg-[#d3c84d] mt-4 mb-4' onClick={() => handleApply()}>Search</button>
         </div>
         
